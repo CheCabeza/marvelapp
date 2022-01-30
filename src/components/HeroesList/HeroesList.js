4f30d1ac1edd328074cd615fb2e71b63424040ee
CheCabeza <chemartinc@gmail.com>
2022-01-30 18:23:39 +0100

    Add loading spinner component

--- src/components/HeroesList/HeroesList.js
+++ src/components/HeroesList/HeroesList.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
-import FlippyCard from "./../flippyCard/FlippyCard";
+import { TailSpin } from "react-loader-spinner";
+
+//Components
+import FlippyCard from "../FlippyCard/FlippyCard";
+
+//Services
 import getHeroes from "./../../services/getHeroes";
 
 export default function HeroesList() {
+  const [loading, setLoading] = useState(false);
   const [heroes, setHeroes] = useState([]);
 
   useEffect(function () {
-    getHeroes().then((heroesData) => setHeroes(heroesData));
+    setLoading(true);
+    getHeroes().then((heroesData) => {
+      setHeroes(heroesData);
+      setLoading(false);
+    });
   }, []);
+
+  if (loading) return <TailSpin />;
 
   return heroes.map(({ id, imgUrl, name }) => (
     <FlippyCard key={id} imgUrl={imgUrl} name={name} id={id} />
